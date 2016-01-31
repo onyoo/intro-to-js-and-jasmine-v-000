da3250f5d682c26f010a4a3c91b870f03ba058ef
onyoo <roberto9m9@gmail.com>
2016-01-31 13:26:47 +0100

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function favIceCream(ice_cream) {
+  return "I love " + ice_cream;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(number) {
+  var number = Math.floor(number);
+  return number;
+}
+
+function theTruth() {
+  return true;
+}
